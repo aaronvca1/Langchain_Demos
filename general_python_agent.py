4dc294efc6b74bb0892191a10cf2b32435b23fd2
Aaron Celeste <50920775+aaronvca1@users.noreply.github.com>
2023-11-07 10:28:38 +0100

    Add files via upload

--- general_python_agent.py
+++ general_python_agent.py
@@ -1,6 +1,5 @@
 from langchain.agents.agent_toolkits import create_python_agent
-#from langchain.tools.python.tool import PythonREPLTool
-from langchain_experimental.tools.python.tool import PythonREPLTool
+from langchain.tools.python.tool import PythonREPLTool
 from langchain.llms.openai import OpenAI
 from langchain.agents.agent_types import AgentType
 from dotenv import load_dotenv
--- thesis_query.py
+++ thesis_query.py
@@ -1,31 +1,29 @@
-import openai
-import pinecone
-from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
-from langchain.llms import OpenAI
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-import streamlit as st
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+import uuid
+from langchain.schema import Document
 
+# Initialize the LLM API Key(s)
 from dotenv import load_dotenv
 load_dotenv()
 
-def load_docs(directory):
-	loader = PyPDFDirectoryLoader(directory)
-	documents = loader.load()
-	return documents
+# Load documents
 
-directory = 'thesis/'
-documents = load_docs(directory)
+loader = UnstructuredPDFLoader("thesis.pdf")
+text = loader.load()
 
-def split_docs(documents, chunk_size=500, chunk_overlap=20):
-	text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-	docs = text_splitter.split_documents(documents)
-	return docs
+text_splitter = RecursiveCharacterTextSplitter(
+	chunk_size = 500,
+	chunk_overlap  = 20,
+	length_function = len,
+	add_start_index = True,
+)
+chunks = text_splitter.split_documents([text[0]])
 
-docs = split_docs(documents)
-print("number of chuncks: " + str(len(docs)))
+print("Total number of split document chunks: " + str(len(chunks)))
+
+#Choose which LLM Provider
 
 which_model = "openai"
 #which_model = "huggingface"
@@ -36,35 +34,20 @@ if(which_model == "openai"):
 if(which_model == "huggingface"):
 	features = 384
 
-embeddings = None
+#Bring in the LLM
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+
+embedding_function = None
 if(which_model == "openai"):
-	embeddings = OpenAIEmbeddings()
+	embedding_function = OpenAIEmbeddings()
 if(which_model == "huggingface"):
-	embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+	embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-pinecone.init(
-	api_key="XXXX",
-	environment="gcp-starter"
-)
-
-index_name = "mcq-creator"
-
-if index_name not in pinecone.list_indexes():
-	pinecone.create_index(
-		name=index_name,
-		metric='cosine',
-		dimension= features
-	)
-	index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-else:
-	index = Pinecone.from_existing_index(index_name, embeddings)
-
-def get_similiar_docs(query, k):
-	similar_docs = index.similarity_search(query, k=k)
-	return similar_docs
-
-from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import Chroma
 from langchain.llms import HuggingFaceHub
+from langchain.llms import OpenAI
 
 llm = None
 if(which_model == "openai"):
@@ -72,21 +55,43 @@ if(which_model == "openai"):
 if(which_model == "huggingface"):
 	llm=HuggingFaceHub(repo_id="bigscience/bloom")
 
-chain = load_qa_chain(llm, chain_type="stuff")
-
-def query_agent(query):
-	relevant_docs = get_similiar_docs(query, k=15)
-	for doc in relevant_docs:
+def ask_the_model(user_input):
+	
+	# Create Chroma Vectorstore database 
+	
+	db = Chroma()
+	if(db._collection.count() == 0):
+		print("Initialized vectorstore with related texts")
+		db = Chroma.from_documents(chunks, embedding_function)
+	else:
+		empty_document = Document(page_content="...", metadata={'source': 'this_doesnt_exist.pdf'})
+		empty_chunks = text_splitter.split_documents([empty_document])
+		db = Chroma.from_documents(empty_chunks, embedding_function)
+		print("Loaded previous vectorstore")
+	
+	# Get chunks of the document which are similar to the query
+	
+	similar_chunks = db.similarity_search(user_input, 15)
+	for chunk in similar_chunks:
 		print(" ")
-		print("relevant doc: " + str(doc))
-	response = chain.run(input_documents=relevant_docs, question=query)
+		print("relevant doc: " + str(chunk))
+	print("number of documents in db: " + str(len(db.get()['documents'])))
+	
+	# Send everything to the LLM
+	
+	query = "You are a chatbot having a conversation with a human. Given the following extracted parts of a long document and a question, create a final answer.\n"
+	query = query + str(similar_chunks) + "\n"
+	query = query + "This is the end of the extracted parts of a long document. The question that you are meant to answer is the following:\n"
+	query = query + str(user_input)
+	response = llm(query)
 	return response
 
+import streamlit as st
 
 st.header("Query your internal documents")
-query = st.text_area("Ask me a question about the attached documents")
+user_input = st.text_area("Ask me a question about the attached documents")
 button = st.button("Submit")
 
 if button:
-	response =  query_agent(query)
+	response = ask_the_model(user_input)
 	st.write(response)
